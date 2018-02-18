253bc8cd211e4a152210b3bedb4a9e5613642f6f
Osric Wilkinson <gitmoose@fluffypeople.com>
2018-02-18 14:31:22 +0000

    New design

--- app/src/main/java/com/fluffypeople/pillclock/PillclockAppWidgetProvider.java
+++ app/src/main/java/com/fluffypeople/pillclock/PillclockAppWidgetProvider.java
@@ -1,12 +1,20 @@
 package com.fluffypeople.pillclock;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.RectF;
 import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -16,26 +24,76 @@ import android.widget.RemoteViews;
 public class PillclockAppWidgetProvider extends AppWidgetProvider {
 
 
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
 
-            Log.d("Provider", "Looking at widget with id: " + appWidgetId);
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            intent.setAction(PillclockApplication.ACTION_RESET);
-            intent.putExtra(PillclockApplication.EXTRA_WIDGET_ID, appWidgetId);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            Calendar lastPill = new GregorianCalendar(2017, 1, 18, 13, 25);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pillclock_appwidget);
-            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            Bitmap clock = drawClock(context, lastPill);
 
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.pillclock_appwidget);
+            view.setBitmap(R.id.imageView, "setImageBitmap", drawClock(context, lastPill));
+            appWidgetManager.updateAppWidget(appWidgetId, view);
         }
     }
+
+
+    private Bitmap drawClock(Context context, Calendar lastPill) {
+        int width, height;
+
+        Calendar now = new GregorianCalendar();
+
+        Bitmap clock = BitmapFactory.decodeResource(context.getResources(), R.drawable.widget0025);
+
+        width = clock.getWidth();
+        height = clock.getHeight();
+
+        Bitmap hand = BitmapFactory.decodeResource(context.getResources(), R.drawable.widget0024);
+        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        p.setColor(0x88888888);
+
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap result = Bitmap.createBitmap(width, height, conf);
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(clock, 0, 0, null);
+
+        float lastPillAngle = (lastPill.get(Calendar.HOUR_OF_DAY) % 12f) * 60f + lastPill.get(Calendar.MINUTE);
+        lastPillAngle = lastPillAngle / (12f * 60f);
+        lastPillAngle *= 360f;
+
+        float nowAngle = (now.get(Calendar.HOUR_OF_DAY) % 12f) * 60f + now.get(Calendar.MINUTE);
+        nowAngle = nowAngle / (12f * 60f);
+        nowAngle *= 360;
+
+        float startAngle = lastPillAngle - 90;
+        float sweepAngle = (nowAngle - lastPillAngle);
+        while (sweepAngle < 0) {
+            sweepAngle += 360;
+        }
+
+        sweepAngle = (sweepAngle % 360);
+
+        canvas.drawArc(new RectF(0, 0, width, height), startAngle, sweepAngle, true, p);
+
+        Log.d("drawClock", "Pill:" + lastPillAngle + ", now " + nowAngle);
+        Log.d("drawClock", "From:" + startAngle + ", sweep " + sweepAngle);
+
+        p.setColorFilter(new PorterDuffColorFilter(0xFF00FF00, PorterDuff.Mode.SRC_IN));
+        canvas.save();
+        canvas.rotate(lastPillAngle, width / 2.0f, height / 2.0f);
+        canvas.drawBitmap(hand, 0, 0, p);
+        canvas.restore();
+
+        p.setColorFilter(new PorterDuffColorFilter(0xFFFFFFFF, PorterDuff.Mode.SRC_IN));
+        canvas.save();
+        canvas.rotate(nowAngle, width / 2.0f, height / 2.0f);
+        canvas.drawBitmap(hand, 0, 0, p);
+        canvas.restore();
+
+        return result;
+    }
+
+
 
 }
--- app/src/main/java/com/fluffypeople/pillclock/PillclockApplication.java
+++ app/src/main/java/com/fluffypeople/pillclock/PillclockApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.content.SharedPreferences;
 
 /**
+ *
  * Created by Osric on 11/02/2018.
  */
 
@@ -13,11 +14,11 @@ public class PillclockApplication extends Application {
     static final String EXTRA_WIDGET_ID = "com.fluffypeople.pillclock.widgetId";
     static final String ACTION_RESET = "com.fluffypeople.pillclock.resetWidget";
 
-    static final String PREFRENCES = "com.fluffypeople.pillclock";
+    static final String PREFERENCES = "com.fluffypeople.pillclock";
 
 
     void setPreference(String key, String value) {
-        SharedPreferences prefs = getSharedPreferences(PREFRENCES, 0);
+        SharedPreferences prefs = getSharedPreferences(PREFERENCES, 0);
 
         prefs.edit()
                 .putString(key, value)
