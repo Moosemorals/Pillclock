1c8cecdec3fb81ab130d4270d987738ed5d566af
Osric Wilkinson <gitmoose@fluffypeople.com>
2018-02-24 11:33:36 +0000

    Remove app compat library

--- app/src/main/java/com/fluffypeople/pillclock/MainActivity.java
+++ app/src/main/java/com/fluffypeople/pillclock/MainActivity.java
@@ -1,10 +1,10 @@
 package com.fluffypeople.pillclock;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,14 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-/*
-    TODO:
-        Alarm/updates
-        Add "are you sure" OK/Cancel to update time
-
- */
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String DATE_FORMAT = "HH:mm dd MMM YYYY";
 
